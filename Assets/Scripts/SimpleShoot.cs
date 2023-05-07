dbc2ec9e477dad1231affb5afc194774349b23b0
DESKTOP-PLERLH9\pablo <pablohernandezjimenez@gmail.com>
2023-05-07 13:09:55 +0100

    Created Enemies

--- Assets/Scripts/SimpleShoot.cs
+++ Assets/Scripts/SimpleShoot.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit;
+using static Unity.VisualScripting.Member;
+
+[AddComponentMenu("Nokobot/Modern Guns/Simple Shoot")]
 public class SimpleShoot : MonoBehaviour
 {
     public int maxammo = 10;
@@ -10,6 +12,7 @@ public class SimpleShoot : MonoBehaviour
 
     public TMPro.TextMeshPro text;
     public GameObject line;
+    public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
     public Transform barrelLocation;
@@ -18,12 +21,27 @@ public class SimpleShoot : MonoBehaviour
     public AudioSource source;
     public AudioClip fire;
     public AudioClip reload;
-    public AudioClip noammo; 
+    public AudioClip noammo;
 
 
     public float shotPower = 100f;
 
     public InputActionReference triggerButtonActionRef;
+    bool isTriggerPressed = false;
+    private void OnEnable()
+    {
+        if (triggerButtonActionRef == null) return;
+
+        triggerButtonActionRef.action.Enable();
+
+        triggerButtonActionRef.action.performed += OnTriggerPress;
+    }
+    private void OnDisable()
+    {
+        if (triggerButtonActionRef == null) return;
+
+        triggerButtonActionRef.action.performed -= OnTriggerPress;
+    }
     void Start()
     {
         if (barrelLocation == null)
@@ -31,7 +49,13 @@ public class SimpleShoot : MonoBehaviour
 
         Reload();
     }
-
+    private void OnTriggerPress(InputAction.CallbackContext context)
+    {
+        if (currentammo > 0)
+            GetComponent<Animator>().SetTrigger("Fire");
+        else
+            source.PlayOneShot(noammo);
+    }
     void Reload()
     {
         currentammo = maxammo;
@@ -40,18 +64,10 @@ public class SimpleShoot : MonoBehaviour
 
     void Update()
     {
-        if (triggerButtonActionRef.action.IsPressed())
-        {
-            if (currentammo > 0)
-                GetComponent<Animator>().SetTrigger("Fire");
-            else
-                source.PlayOneShot(noammo);
-        }
-
         if (Vector3.Angle(transform.up, Vector3.up) > 100 && currentammo < maxammo)
             Reload();
 
-        text.text = currentammo.ToString();
+        if(text) text.text = currentammo.ToString();
     }
 
     public void Shoot()
@@ -64,7 +80,9 @@ public class SimpleShoot : MonoBehaviour
         source.PlayOneShot(fire);
 
         GameObject tempFlash;
-       tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+        if (bulletPrefab)
+            Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower, ForceMode.VelocityChange);
+        tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
 
         RaycastHit hitInfo;
         bool hasHit = Physics.Raycast(barrelLocation.position, barrelLocation.forward, out hitInfo, 100);
@@ -72,26 +90,25 @@ public class SimpleShoot : MonoBehaviour
         if (hasHit)
             hitInfo.collider.SendMessageUpwards("Dead", hitInfo.point, SendMessageOptions.DontRequireReceiver);
 
-        if(line)
+        if (line)
         {
             GameObject liner = Instantiate(line);
-            liner.GetComponent<LineRenderer>().SetPositions(new Vector3[] { barrelLocation.position , barrelLocation.position + barrelLocation.forward * 100 });
+            liner.GetComponent<LineRenderer>().SetPositions(new Vector3[] { barrelLocation.position, barrelLocation.position + barrelLocation.forward * 100 });
 
             Destroy(liner, 0.5f);
         }
 
-       // Destroy(tempFlash, 0.5f);
+        // Destroy(tempFlash, 0.5f);
         //  Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
-       
+
     }
 
     void CasingRelease()
     {
-         GameObject casing;
+        GameObject casing;
         casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
         casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
         casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
     }
-
 
 }
